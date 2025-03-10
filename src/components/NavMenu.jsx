3c477d93f1cfcaa10e5f4c1fb698ca792c18bff4
Łukasz Grzecznik <lukaszgrzecznik@gmail.com>
2025-03-10 23:25:58 +0100

    Added Navigation Menu component

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,11 @@
+export const NavMenu = () => {
+    return(
+        <nav>
+            <div id={"nav-image"}></div>
+            <a id={"nav-home"}>Home</a>
+            <a id={"nav-user"}>User Panel</a>
+            <a id={"nav-admin"}>Admin Panel</a>
+            <a id={"nav-login"}>Log in</a>
+        </nav>
+    );
+};
