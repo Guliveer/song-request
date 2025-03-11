4e45f2db196b2e003d75cf2bd2053061afdc79d5
Łukasz Grzecznik <lukaszgrzecznik@gmail.com>
2025-03-11 22:25:17 +0100

    Implemented MUI, converted Menu from column to row

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,11 +1,39 @@
-export const NavMenu = () => {
-    return(
-        <nav>
-            <div id={"nav-image"}></div>
-            <a id={"nav-home"}>Home</a>
-            <a id={"nav-user"}>User Panel</a>
-            <a id={"nav-admin"}>Admin Panel</a>
-            <a id={"nav-login"}>Log in</a>
-        </nav>
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+
+const NavMenu = () => {
+    return (
+        <AppBar
+            position="fixed"
+            sx={{
+                width: "100vw",
+                height: "6rem",
+                left: 0,
+                top: 0,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                zIndex: 1200,
+                backgroundColor: "grey",
+                flexWrap: "nowrap"
+            }}
+        >
+            <Toolbar sx={{ display: "flex", alignItems: "center", flexDirection: "row", flexWrap: "nowrap", justifyContent: "space-between"}}>
+                <Typography variant="h5" component="div" sx={{display: "block", padding: "1rem"}}>
+                    Logo
+                </Typography>
+                <Box sx={{ display: "flex", flexDirection: "row", width: "100%"}}>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">User Panel</Button>
+                    <Button color="inherit">Admin Panel</Button>
+                    <Button color="inherit">Log in</Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 };
+
+export default NavMenu;
+
+
