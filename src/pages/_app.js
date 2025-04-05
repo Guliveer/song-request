2998ed0856fbe904ab057b52413c91ff02d52f49
lifeoverthinker <96396847+lifeoverthinker@users.noreply.github.com>
2025-04-05 21:35:36 +0200

    favicon + pages titles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,17 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/styles/theme';
 import '@/styles/globals.css';
 import NavMenu from '@/components/NavMenu';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+    useEffect(() => {
+        const link = document.querySelector("link[rel~='icon']") || document.createElement('link');
+        link.rel = 'icon';
+        link.type = 'image/png';
+        link.href = '/favicon.ico';
+        document.head.appendChild(link);
+    }, []);
+
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
