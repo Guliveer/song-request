0b732481ae60b5e69309884f6e4f954356676ca5
lifeoverthinker <96396847+lifeoverthinker@users.noreply.github.com>
2025-06-01 20:51:18 +0200

    Footer and page horizontal responsivity fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,32 @@
-import { UserProvider } from "@/context/UserContext";
-import { ThemeProvider } from '@mui/material/styles';
+import {UserProvider} from "@/context/UserContext";
+import {ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/styles/theme';
 import '@/styles/globals.css';
 import NavMenu from '@/components/NavMenu';
 import Footer from '@/components/Footer';
-import AddSongForm from '@/components/AddSongForm'; // FAB to add song
+import AddSongForm from '@/components/AddSongForm';
 import PropTypes from "prop-types";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
+import Box from "@mui/material/Box"; // DODAJ TEN IMPORT
 
-export default function App({ Component, pageProps }) {
-
+export default function App({Component, pageProps}) {
     return (
         <ThemeProvider theme={theme}>
             <UserProvider>
-                <CssBaseline />
-                <NavMenu />
-                <Component {...pageProps} />
-                <Footer />
+                <CssBaseline/>
+                <Box
+                  sx={{
+                    minHeight: "100vh",
+                    display: "flex",
+                    flexDirection: "column",
+                    overflowX: "hidden", // zapobiega przewijaniu w poziomie
+                  }}
+                >
+                  <NavMenu/>
+                  <Component {...pageProps} />
+                  <Footer/>
+                </Box>
             </UserProvider>
         </ThemeProvider>
     );
