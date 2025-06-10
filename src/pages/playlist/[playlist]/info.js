eac99d113b54077345f999eaa019859a03bea7ab
Oliwer Pawelski <oliwer.pawelski@gmail.com>
2025-06-10 13:22:10 +0200

    Preparation for info page implementation

--- src/pages/playlist/[playlist]/info.js
+++ src/pages/playlist/[playlist]/info.js
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import {
     getCurrentUser,
     getJoinedPlaylists,
-    getPlaylistData
+    getPlaylistData, getPlaylistModerators
 } from "@/utils/actions";
 import {
     Box,
     CircularProgress,
 } from "@mui/material";
+import SetTitle from "@/components/SetTitle";
+import PlaylistMenu from "@/components/PlaylistManagement/PlaylistMenu";
 
 export default function ManagePlaylist() {
     const router = useRouter();
@@ -17,7 +19,6 @@ export default function ManagePlaylist() {
     const [playlistData, setPlaylistData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [currentUser, setCurrentUser] = useState(null);
-    const [hasJoined, setHasJoined] = useState(null);
 
     useEffect(() => {
         const fetchCurrentUser = async () => {
@@ -28,6 +29,7 @@ export default function ManagePlaylist() {
         fetchCurrentUser();
     }, []);
 
+
     useEffect(() => {
         if (!router.isReady || !playlist) return;
 
@@ -36,11 +38,15 @@ export default function ManagePlaylist() {
                 const data = await getPlaylistData(playlistId);
 
                 if (currentUser && data) {
+                    // Check if the current user has joined the playlist
                     const joinedPlaylists = await getJoinedPlaylists(currentUser.id);
                     const joinStatus = joinedPlaylists.includes(data?.id);
-                    setHasJoined(joinStatus);
 
-                    if (data.is_public === false && data.method === 'id' && !joinStatus) {
+                    // Check if the current user is a moderator or host
+                    const moderators = await getPlaylistModerators(data?.id);
+                    const allowAccess = Object.keys(moderators).includes(currentUser.id) || data.host === currentUser.id;
+
+                    if ((data.is_public === false && data.method === 'id' && !joinStatus) || !allowAccess) {
                         console.warn("You cannot access this playlist right now.");
                         setPlaylistData(null);
                         setLoading(false);
@@ -90,11 +96,23 @@ export default function ManagePlaylist() {
         );
     }
 
-    const isHost = currentUser?.id === playlistData.host; //? Must be here - at the end of all loadings and checks
-
     return (
-        <div>
-            {playlistData.name} - Info
-        </div>
+        <>
+            <SetTitle text={`Playlist Info - ${playlistData.name}`} />
+
+            {/* Menu */}
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                padding: '1rem 2rem',
+            }}>
+                <PlaylistMenu playlistId={playlistData.id} />
+            </Box>
+
+            <Box>
+                {/* TODO */}
+            </Box>
+        </>
     )
 }
