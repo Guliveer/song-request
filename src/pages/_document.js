ba18b5a25c2bc7785aa1c96d4d4be9b55523ae88
Oliwer Pawelski <oliwer.pawelski@gmail.com>
2025-06-27 16:41:39 +0200

    Set dark mode as default

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html lang="en">
+        <Html lang="en" className={"dark"}>
             <Head>
                 <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
                 <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
